5a3ac349be130f5c885cd2fcddbb2384bcb9078e
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-10-18 00:26:09 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import '../assets/styles/Header.css';
 
 export default function Header() {
   return (
-    <header className='header'>
-      <img className='header--image' src={trollFace} alt='Troll Face' />
-      <p className='header--title'>Meme Generator</p>
-      <p className='header--project'>React Course - Project 3</p>
+    <header className="header">
+      <img className="header--image" src={trollFace} alt="Troll Face" />
+      <p className="header--title">Meme Generator</p>
+      <p className="header--project">React Project</p>
     </header>
   );
 }
